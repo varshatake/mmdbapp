5f710fd185fdcf619b551cc04980c2affdeb5f3b
varshatake <varshagore108@gmail.com>
2024-01-25 23:51:37 +0530

    Latest changes

--- mmdb/src/app/mdetail/mdetail.component.ts
+++ mmdb/src/app/mdetail/mdetail.component.ts
@@ -5,30 +5,34 @@ import { MovieserService } from 'src/services/movieser.service';
 @Component({
   selector: 'app-mdetail',
   templateUrl: './mdetail.component.html',
-  styleUrls: ['./mdetail.component.css']
+  styleUrls: ['./mdetail.component.css'],
 })
-export class MdetailComponent implements OnInit{
-  movieresult:any;
-  constructor(private _service:MovieserService,private route: ActivatedRoute){
-
-  }
+export class MdetailComponent implements OnInit {
+  movieresult: any;
+  constructor(
+    private _service: MovieserService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    
+    this.route.params.subscribe((params) => {
+      this.getmoviedetails(params['title']);
+    });
   }
 
-  getmoviedetails(movieName:any) {
-    return this._service.getmovieDetails(movieName).subscribe((movieResponse) => {
-      this.movieresult = movieResponse.results;
-      console.log('Movie details response :', movieResponse);
-    }); 
+  getmoviedetails(movieName: any) {
+    return this._service
+      .getmovieDetails(movieName)
+      .subscribe((movieResponse) => {
+        this.movieresult = movieResponse;
+        console.log('Movie details response :', movieResponse);
+      });
   }
 
-  ontitleclick(){
+  ontitleclick() {
     let title = this.route.snapshot.params['title'];
-    this._service.getmovieDetails(title).subscribe((res:any) => {
+    this._service.getmovieDetails(title).subscribe((res: any) => {
       console.log('post deatils', res);
     });
-  
-}
+  }
 }
--- mmdb/src/services/movieser.service.ts
+++ mmdb/src/services/movieser.service.ts
@@ -29,7 +29,7 @@ export class MovieserService {
     );
   }
 
-  getmovieDetails(title:any): Observable<any> {
-    return this._http.get(`https://api.themoviedb.org/3/movie/$%7B`+title+`%7D?api_key=5c06fed2cdf4dfcdab132d9e67c1c2e7&language=en-US`,title);
+  getmovieDetails(movieId:any): Observable<any> {
+    return this._http.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=5c06fed2cdf4dfcdab132d9e67c1c2e7&language=en-US`);
   }
 }
